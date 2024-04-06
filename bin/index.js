a2700b2223af9a727e0b53fd3f7ca68942feac8b
高昇 <xiaogao235@gmail.com>
2024-04-06 15:16:23 +0800

    fix: read displayName error

--- bin/index.js
+++ bin/index.js
@@ -9,19 +9,19 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
     const explorerSync = cosmiconfigSync('displayName');
     const result = explorerSync.load(path.join(process.cwd(), 'app.json'));
-    if (result) {
-        await setDisplayName(result);
+    if (result && result.config && result.config.displayName) {
+        setDisplayName(result.config.displayName);
         console.log('\x1b[32m%s\x1b[0m', "Sync app.json displayName success!");
     } else {
         console.error('\x1b[31m%s\x1b[0m', 'Please enter a display name');
     }
-}else {
-    await setDisplayName(args[0]);
+} else {
+    setDisplayName(args[0]);
     console.log('\x1b[32m%s\x1b[0m', "Set display name success!");
 }
 
-async function setDisplayName(name) {
+function setDisplayName(name) {
     common.setDisplayName(name);
     ios.setPlistValue('CFBundleDisplayName', name);
-    await android.setString('app_name', name);
+    android.setString('app_name', name);
 }
