2ba775793e25a08e9f49491155d89f411506c71d
高昇 <xiaogao235@gmail.com>
2024-04-06 15:06:42 +0800

    add sync app.json display name

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,23 @@
+#!/usr/bin/env node
+
+const {cosmiconfigSync} = require('cosmiconfig');
+const path = require('path');
 const {ios, android, common} = require('@raydeck/react-native-utilities');
 
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-    console.error('\x1b[31m%s\x1b[0m', 'Please enter a display name');
-    return;
+    const explorerSync = cosmiconfigSync('displayName');
+    const result = explorerSync.load(path.join(process.cwd(), 'app.json'));
+    if (result) {
+        await setDisplayName(result);
+        console.log('\x1b[32m%s\x1b[0m', "Sync app.json displayName success!");
+    } else {
+        console.error('\x1b[31m%s\x1b[0m', 'Please enter a display name');
+    }
+}else {
+    await setDisplayName(args[0]);
+    console.log('\x1b[32m%s\x1b[0m', "Set display name success!");
 }
 
 async function setDisplayName(name) {
@@ -12,6 +25,3 @@ async function setDisplayName(name) {
     ios.setPlistValue('CFBundleDisplayName', name);
     await android.setString('app_name', name);
 }
-
-await setDisplayName(args[0]);
-console.log('\x1b[32m%s\x1b[0m', "set display name success!");
