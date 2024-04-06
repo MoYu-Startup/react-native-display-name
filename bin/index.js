c565db4c9589ea3afae3197c89242a795be46094
高昇 <xiaogao235@gmail.com>
2024-04-06 15:24:43 +0800

    fix: sync app.json don't update app.json

--- bin/index.js
+++ bin/index.js
@@ -10,18 +10,18 @@ if (args.length === 0) {
     const explorerSync = cosmiconfigSync('displayName');
     const result = explorerSync.load(path.join(process.cwd(), 'app.json'));
     if (result && result.config && result.config.displayName) {
-        setDisplayName(result.config.displayName);
+        setDisplayName(result.config.displayName, true);
         console.log('\x1b[32m%s\x1b[0m', "Sync app.json displayName success!");
     } else {
         console.error('\x1b[31m%s\x1b[0m', 'Please enter a display name');
     }
 } else {
-    setDisplayName(args[0]);
+    setDisplayName(args[0], false);
     console.log('\x1b[32m%s\x1b[0m', "Set display name success!");
 }
 
-function setDisplayName(name) {
-    common.setDisplayName(name);
+function setDisplayName(name, isSync) {
+    !isSync && common.setDisplayName(name);
     ios.setPlistValue('CFBundleDisplayName', name);
     android.setString('app_name', name);
 }
